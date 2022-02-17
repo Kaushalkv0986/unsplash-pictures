de16431f459f64f1e72d920bcc8c4a224a462893
kaushal Kishor Vidyarthi <kaushalkv0986@gmail.com>
2022-02-17 14:13:44 +0530

    Updates

--- src/api/unsplash.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-  baseURL: 'https://api.unsplash.com',
-  headers: {
-    Authorization: 'Client-ID 3gf20idCA0FjZmHMtTgKcnyPtZLWzPP9iWfsLELpK4g'
-  }
-});
